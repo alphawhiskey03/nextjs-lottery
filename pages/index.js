559d7871d4782ecba476976f7f34e9d0dd1316a8
vignesh <vignesh@vigneshs-MacBook-Air.local>
2022-10-08 19:58:52 +0530

    fix(frontend): removed next/image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 // import Header from "../components/ManualHeader";
 import Header from "../components/AutoHeader";
 import styles from "../styles/Home.module.css";
